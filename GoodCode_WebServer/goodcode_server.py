9454693ef11b0ea2a39bbec9eac05b8a502ab308
Minh-Quan Nguyen <Minh-Quan_Nguyen@neimanmarcus.com>
2018-02-13 07:30:59 -0600

    Updated Directory Pathway and Added Comments

--- GoodCode_WebServer/goodcode_server.py
+++ GoodCode_WebServer/goodcode_server.py
@@ -1,10 +1,31 @@
-from flask import Flask
+# ----------------------------------- Import Needed Libraries ----------------------------------- #
+import sys
 
+from flask import Flask, render_template
+
+# ------------------------------------ Initialize Flask App. ------------------------------------ #
 app = Flask(__name__)
 
+# ------------------------------------ Web Server Controller ------------------------------------ #
+'''
+Endpoint    : localhost:PORT/
+Purpose     : Homepage of GoodCode
+'''
 @app.route('/')
 def homepage():
-    return "Hello World!"
+    return render_template('goodcode_homepage.html')
 
+# ------------------------------------ Start Web Server App. ------------------------------------ #
 if __name__ == '__main__':
-    app.run(port=8080)
+    
+    # ----------------- Grab Port Number from the User ----------------- #
+    # --- Take the input port number and store it in a local variable
+    # --- If port is not specified, default to port 80
+    try:
+        inputPort = sys.argv[1]
+    except:
+        print("No port specified. Defaulting to Port 80.")
+        inputPort = 80
+
+    # ----------------- Run the Web Server Application ----------------- #
+    app.run(port=inputPort)
